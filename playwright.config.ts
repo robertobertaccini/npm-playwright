bbd0608f8ec501185bd8b9e964ae9d1b06e904f1
Roberto Bertaccini <roberto.bertaccini@gmail.com>
2025-04-23 19:02:46 +0200

    slowmo

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,15 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  timeout: 120_000,
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -31,6 +39,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false,
+    launchOptions: {
+    slowMo: 3000,
+},
   },
 
   /* Configure projects for major browsers */
